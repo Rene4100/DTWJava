d53e8f4abb644f56b13d4916fbdafb3a7afa2aa4
Rene Mejer Lauritsen <rlauri13@student.aau.dk>
2017-04-20 09:33:59 +0200

    added the project files, together with the DTW algo

---
+++ src/dk/rml/DTW.java
@@ -0,0 +1,43 @@
+package dk.rml;
+
+public class DTW {
+    private static int inf = 9000;
+
+    public static double DTWDistance(double[] s, double[] t){
+        double[][] dtw = new double[s.length + 1][t.length + 1];
+
+        dtw[0][0] = 0;
+        for (int i = 1; i <s.length + 1; i++) {
+            dtw[i][0] = inf;
+        }
+        for (int i = 1; i <t.length + 1; i++) {
+            dtw[0][i] = inf;
+        }
+
+        for (int i = 1; i < s.length + 1; i++) {
+            for (int j = 1; j < t.length + 1; j++) {
+                double cost = distanceD(s[i - 1], t[j - 1]);
+                dtw[i][j] = cost + minimum(dtw[i - 1][j],      //match
+                                           dtw[i][j - 1],      //insertion
+                                           dtw[i - 1][j - 1]); //deletion
+            }
+        }
+        return dtw[s.length][t.length];
+    }
+
+    private static double distanceD(double a, double b) {
+        return Math.abs(a-b);
+    }
+
+    ///Finds the lowest value and returns it
+    private static double minimum(double i, double i1, double i2) {
+        if (i < i1) {
+            if (i < i2) {
+                return i;
+            }
+        } else if (i1 < i2) {
+            return i1;
+        }
+        return i2;
+    }
+}
---
+++ src/dk/rml/Main.java
@@ -0,0 +1,11 @@
+package dk.rml;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] array1 = {1, 2, 3, 5, 5, 5, 6};
+        double[] array2 = {1, 1, 2, 2, 3, 5 };
+
+        System.out.println(DTW.DTWDistance(array1, array2));
+    }
+
+}
